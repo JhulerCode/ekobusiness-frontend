7f620d4da0d1dcec38ab120bbc9704049d5a52e7
Jhuler Aguirre <jhuler.code@gmail.com>
2025-12-04 17:10:18 -0500

    feat: en invetario hacer consultas con filtros de fecha, nombre y categoria

--- src/pinia/auth.js
+++ src/pinia/auth.js
@@ -8,7 +8,7 @@ export const useAuth = defineStore('auth', {
     state: () => ({
         token: null,
         usuario: {},
-        app_version: '1.7.4',
+        app_version: '1.7.5',
 
         menu: [
             {
--- src/views/logistica_entrada/inventario/vInventarioArticulos.vue
+++ src/views/logistica_entrada/inventario/vInventarioArticulos.vue
@@ -4,7 +4,6 @@
             <strong>Inventario</strong>
 
             <div class="buttons">
-                <JdInput type="date" v-model="vista.f2" style="width: 10rem" />
                 <JdButton
                     @click="loadInventario"
                     text="Buscar"
@@ -13,12 +12,18 @@
             </div>
         </div>
 
-        <JdTable :columns="columns" :datos="vista.inventario || []" />
+        <JdTable
+            :columns="columns"
+            :datos="vista.inventario || []"
+            :configFiltros="openConfigFiltros"
+        />
     </div>
+
+    <mConfigFiltros v-if="useModals.show.mConfigFiltros" />
 </template>
 
 <script>
-import { JdTable, JdInput, JdButton } from '@jhuler/components'
+import { JdTable, JdButton, mConfigFiltros } from '@jhuler/components'
 
 import { useAuth } from '@/pinia/auth'
 import { useModals } from '@/pinia/modals'
@@ -33,8 +38,8 @@ import { jmsg } from '@/utils/swal'
 export default {
     components: {
         JdTable,
-        JdInput,
         JdButton,
+        mConfigFiltros,
     },
     data: () => ({
         useAuth: useAuth(),
@@ -45,18 +50,28 @@ export default {
 
         vista: {},
 
+        tableName: 'vInventarioArticulos',
         columns: [
+            {
+                id: 'fecha',
+                title: 'Fecha',
+                type: 'date',
+                show: false,
+            },
             {
                 id: 'nombre',
                 title: 'Nombre',
+                type: 'text',
                 width: '30rem',
                 show: true,
                 seek: true,
                 sort: true,
             },
             {
-                id: 'categoria1.nombre',
+                id: 'categoria',
                 title: 'Categoría',
+                prop: 'categoria1.nombre',
+                type: 'select',
                 width: '12rem',
                 show: true,
                 seek: true,
@@ -84,12 +99,18 @@ export default {
     }),
     created() {
         this.vista = this.useVistas.vInventarioArticulos
+        this.initFiltros()
+        this.useAuth.setColumns(this.tableName, this.columns)
 
         if (this.vista.loaded) return
     },
     methods: {
+        initFiltros() {
+            this.columns[0].op = 'Es igual o anterior a'
+            this.columns[0].val = dayjs().format('YYYY-MM-DD')
+        },
         checkDatos() {
-            if (!this.vista.f2) {
+            if (!this.columns[0].val) {
                 jmsg('error', 'Ingrese la fecha límite')
                 return true
             }
@@ -102,9 +123,6 @@ export default {
                     tipo: { op: 'Es', val: 1 },
                 },
                 incl: ['categoria1'],
-                transaccion_items: {
-                    f2: { op: 'Es', val: this.vista.f2 },
-                },
             }
 
             this.useAuth.updateQuery(this.columns, this.vista.qry)
@@ -123,6 +141,40 @@ export default {
             if (res.code != 0) return
 
             this.vista.inventario = res.data
+        },
+
+        async openConfigFiltros() {
+            await this.loadCategorias()
+
+            const cols = this.columns.filter((a) => a.id != 'unidad' && a.id != 'cantidad')
+            cols.find((a) => a.id == 'categoria').lista = this.vista.articulo_categorias
+
+            const send = {
+                table: this.tableName,
+                cols,
+                reload: this.loadInventario,
+            }
+
+            this.useModals.setModal('mConfigFiltros', 'Filtros', null, send, true)
+        },
+
+        async loadCategorias() {
+            const qry = {
+                cols: ['nombre'],
+                fltr: {
+                    tipo: { op: 'Es', val: 1 },
+                    activo: { op: 'Es', val: true },
+                },
+            }
+
+            this.vista.articulo_categorias = []
+            this.useAuth.setLoading(true, 'Cargando...')
+            const res = await get(`${urls.articulo_categorias}?qry=${JSON.stringify(qry)}`)
+            this.useAuth.setLoading(false)
+
+            if (res.code != 0) return
+
+            this.vista.articulo_categorias = res.data
         },
     },
 }
